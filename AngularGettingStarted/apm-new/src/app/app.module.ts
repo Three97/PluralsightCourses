169451a4466aa2eeeac4c95135e1e245c2a99726
Jason Fedler <Jason.Fedler@gmail.com>
2023-12-01 16:19:23 -0500

    adds filtering as well as get/set for filter;

--- AngularGettingStarted/apm-new/src/app/app.module.ts
+++ AngularGettingStarted/apm-new/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
